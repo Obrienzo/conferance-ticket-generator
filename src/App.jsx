be8eaeb4352c93d6b35a1b0dc6bce762254721c5
Disney <disneyconstant996@gmail.com>
2025-05-22 12:32:17 +0300

    Add background styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import IntroPage from "./IntroPage.jsx";
 function App() {
 
   return (
-    <main className="min-h-screen grid place-content-center bg-[url('/src/assets/images/background-mobile.png')] md:bg-[url('/src/assets/images/background-tablet.png')] lg:bg-[url('/src/assets/images/background-desktop.png')] bg-cover bg-center text-neutral-100 p-5">
+    <main className="min-h-screen grid place-content-center text-neutral-100 p-5 relative">
+    
       <div className="mb-4.5">
                 <img className="mx-auto" src="/src/assets/images/logo-full.svg" alt="Page logo" />
             </div>
-    <IntroPage />
+      <IntroPage />
+
+      {/* Adding the additional styling elements */}
+      <img className="absolute bottom-0 -z-10 lg:invisible" src="/src/assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg" alt="" />
+      <img className="absolute bottom-0 -z-10 invisible lg:visible" src="/src/assets/images/pattern-squiggly-line-bottom-desktop.svg" alt="" />
+      <img className="absolute right-0 -z-20" src="/src/assets/images/pattern-squiggly-line-top.svg" alt="" />
     </main>
   )
 }
